27d731ff87b9a1e3cb7eb1097b6c424a372550f0
Van Truong <90664293+lqvt2002@users.noreply.github.com>
2023-06-29 01:12:59 +0700

    Update config.php

--- database/config.php
+++ database/config.php
@@ -1,6 +1,6 @@
 <?php
-    const  HOST ="localhost";
-    const username = "root";
-    const password = "mysql";
-    const database = "sale";
+    const  HOST ="us-cdbr-east-06.cleardb.net";
+    const username = "b80b79f5de4fd9";
+    const password = "46f53e94";
+    const database = "heroku_3e0958e54840a44";
     const private_key= "duwjndc9(*&7fhnuc7&";
